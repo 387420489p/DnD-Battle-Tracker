575f0e176f5d3230cb786530cb6e983e57a0ffbc
PTR <majercsikpeter@gmail.com>
2022-10-03 10:10:25 +0200

    add PC stats (hardcode)

--- fight_tracker.py
+++ fight_tracker.py
@@ -1,5 +1,9 @@
 #TODO földre kerülés PC-knek DONE
 #TODO exception handling
+#TODO neveket, dmg-nél capital-ba konvertálni
+#TODO npc darabszám szorzás
+#TODO npc random initiative
+#TODO ha megval valaki írja ki, hogy meghalt !!!!!!!
 initiative = []
 fighters = []
 
@@ -22,7 +26,10 @@ def get_started():
             pc = True
         else:
             pc = False
-        nev = input("Name: ")
+        nev = input("Name: ").lower()
+        if nev in ["baldwin", "bob", "bog", "luna", "doni", "urist", "beni"]:
+            pc_character(nev)
+            break
         hp = int(input("HP: "))
         ac = int(input("AC: "))
         ini = int(input("Initiative: "))
@@ -55,12 +62,12 @@ def get_new_ini():
 def fight():
     global initiative, fighters
     print("=============================" * len(fighters))
-    print("\nInitiative: ", end=" ")
+    print("\nInitiative: ")
     for i in initiative:
         if i[2] == True and i[1] <= 0:
-            print(f"Name: {i[0]}, PLAYER DOWN!||", end=" ")
+            print(f"{i[0]}, PLAYER DOWN!||")
         else:
-            print(f"Name: {i[0]}, HP: {i[1]}, AC: {i[3]} ||", end=" ")
+            print(f"{i[0]}, HP: {i[1]}, AC: {i[3]} ||")
     print("\n")
     print("=============================" * len(fighters))
 
@@ -76,5 +83,50 @@ def fight():
     print(r"""        \ """)
     get_new_ini()
 
+def pc_character(name):
+    if name == "baldwin":
+        ac = 19
+        hp = 79
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+    elif name == "bob":
+        ac = 20
+        hp = 77
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+    elif name == "luna":
+        ac = 13
+        hp = 54
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+    elif name == "bog":
+        ac = 17
+        hp = 47
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+    elif name == "doni":
+        ac = 15
+        hp = 80
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+    elif name == "urist":
+        ac = 14
+        hp = 71
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+    elif name == "beni":
+        ac = 10
+        hp = 49
+        ini = int(input("Initiative: "))
+        pc = True
+        get_character(name, hp, ac, ini, pc)
+
+    get_started()
 
 get_started()
